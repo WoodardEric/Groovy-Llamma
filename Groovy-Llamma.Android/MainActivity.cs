94212fa5bef642d8740e08700d4af0c3b41f6417
WoodyEric <woodard.eric09@gmail.com>
2021-07-28 19:28:37 -0700

    add icons to android project

--- Groovy-Llamma.Android/MainActivity.cs
+++ Groovy-Llamma.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace Groovy_Llamma.Droid
 {
-    [Activity(Label = "Groovy_Llamma", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Groovy   Llamma", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
