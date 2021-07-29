cb90dbecb534e73ef951a649ac7ad6440f8604a2
WoodyEric <woodard.eric09@gmail.com>
2021-07-28 19:30:38 -0700

    implement AudioPlayer for android

---
+++ Groovy-Llamma/AudioPlayerService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Groovy_Llamma.Services
+{
+    public interface AudioPlayerService
+    {
+        void Play(string audioPath);
+        void Play();
+        void Pause();
+        Action OnFinishedPlaying { get; set; }
+    }
+}
+
