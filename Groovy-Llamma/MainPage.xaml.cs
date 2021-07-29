31c8a82f4d90b0a891563072b337e9fe14a8c6cc
WoodyEric <woodard.eric09@gmail.com>
2021-07-28 19:32:05 -0700

    update gui

--- Groovy-Llamma/MainPage.xaml.cs
+++ Groovy-Llamma/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Groovy_Llamma.Services;
+using Groovy_Llamma.ViewModels;
 
 namespace Groovy_Llamma
 {
@@ -13,6 +15,7 @@ namespace Groovy_Llamma
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = new AudioPlayerViewModel(DependencyService.Get<AudioPlayerService>());
         }
     }
 }
